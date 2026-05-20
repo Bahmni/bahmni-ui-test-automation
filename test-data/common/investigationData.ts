c37bd2754cf5824c4269f8a91d1a3a511ed52913
Narmadha <narmadha.ramaswamy@thoughtworks.com>
2026-05-20 11:49:06 +0530

    Common data for std and lite

--- test-data/common/investigationData.ts
+++ test-data/common/investigationData.ts
@@ -7,25 +7,25 @@ import { faker } from '@faker-js/faker';
 
 const INVESTIGATIONS_PANEL = [
   'Complete blood count (auto) (Panel)',
-  'Blood grouping test (Panel)',
-  'Ceruloplasmin test (Panel)',
-  'Alpha fetoprotein test (Panel)',
-  'Diabetes panel (Panel)',
+  'Liver function tests (Panel)',
+  'Lipid panel (Panel)',
+  'Bone marrow test (Panel)',
+  'Cancer marker panel (Panel)',
 ];
 
 const INVESTIGATIONS_SINGLE = [
-  'Hemoglobin in arterial blood (mass/volume)',
-  'Ammonia measurement in blood',
-  'Benzodiazepines screen, urine',
-  'Arterial blood lactate measurement',
+  'Absolute eosinophil count',
+  'White blood cells',
+  'Urine bile pigment test',
+  'Fasting blood glucose measurement (mg/dL)',
 ];
 
 const INVESTIGATIONS_RADIOLOGY = [
-  'X-ray, chest',
+  'X-ray of chest, four views',
   'X-ray of bilateral ribs, 2 views',
   'X-ray of right elbow, two views',
-  'Echocardiogram',
-  'Transthoracic echocardiogram',
+  'fluoroscopy, esophagus',
+  'CT scan, pelvis',
 ];
 
 const PROCEDURES = ['Electrocautery procedure', 'Colonoscopy', 'Extraction of cataract'];

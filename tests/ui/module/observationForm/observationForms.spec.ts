a5a847aecf84885da541bc451ad6b2fb29a00c45
Narmadha <narmadha.ramaswamy@thoughtworks.com>
2026-07-08 10:48:03 +0530

    tag to regression

--- tests/ui/module/observationForm/observationForms.spec.ts
+++ tests/ui/module/observationForm/observationForms.spec.ts
@@ -5,7 +5,7 @@ import { diabetesProgressFaker } from '../../../../test-data/common/diabetesProg
 import { malariaFaker } from '../../../../test-data/common/malariaData';
 import { vitalsFaker } from '../../../../test-data/common/vitalsData';
 
-test.describe('Observation form Tests', () => {
+test.describe('Observation form Tests', { tag: ['@regression'] }, () => {
   test('Add admission letter observation form in consultation', async ({ clinicalSetup }) => {
     test.setTimeout(60000);
     const { actions, bahmni, page } = clinicalSetup;
